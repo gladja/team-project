53762ada07a73e830ebd5faa025a1bd27125b78c
Robertw8 <oniedgaf@gmail.com>
2023-05-06 22:44:15 +0300

    updated mobile menu + modal + refactored catalog

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -2,8 +2,19 @@ export const menuRefs = {
   open: document.querySelector('.btn-menu'),
   close: document.querySelector('.menu-close-btn'),
   menu: document.querySelector('.mobile-menu'),
+  links: document.querySelectorAll('.menu-link'),
 };
+const body = document.querySelector('body');
 
 export function toggleMenu() {
   menuRefs.menu?.classList.toggle('menu-animation');
+  body?.classList.toggle('overflow-hidden');
+
+  //? Закрытие меню при скролле на якорь
+  menuRefs.links?.forEach(link => {
+    link.addEventListener('click', () => {
+      menuRefs.menu?.classList.remove('menu-animation');
+      body?.classList.remove('overflow-hidden');
+    });
+  });
 }
